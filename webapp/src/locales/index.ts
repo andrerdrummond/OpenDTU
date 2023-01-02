ed461b8ba0e985100a1ce69cfee98884525ed427
André Drummond <andrerdrummond@gmail.com>
2023-01-02 17:06:34 -0300

    Added Brazil Portuguese translation

--- webapp/src/locales/index.ts
+++ webapp/src/locales/index.ts
@@ -2,23 +2,27 @@ import type { I18nOptions } from "vue-i18n";
 import en from './en.json'
 import de from './de.json'
 import fr from './fr.json'
+import pt_br from './pt_br.json'
 
 export enum Locales {
     EN = 'en',
     DE = 'de',
     FR = 'fr',
+    PT_BR = 'pt_br',
 }
 
 export const LOCALES = [
     { value: Locales.EN, caption: 'English' },
     { value: Locales.DE, caption: 'Deutsch' },
     { value: Locales.FR, caption: 'Français' },
+    { value: Locales.PT_BR, caption: 'Português Brasil' },
 ]
 
 export const messages: I18nOptions["messages"] = {
     [Locales.EN]: en,
     [Locales.DE]: de,
     [Locales.FR]: fr,
+    [Locales.PT_BR]: pt_br,
 };
 
 export const dateTimeFormats: I18nOptions["datetimeFormats"] = {
@@ -45,6 +49,17 @@ export const dateTimeFormats: I18nOptions["datetimeFormats"] = {
         }
     },
     [Locales.FR]: {
+        'datetime': {
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric',
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric',
+            hour12: false
+        }
+    },
+    [Locales.PT_BR]: {
         'datetime': {
             hour: 'numeric',
             minute: 'numeric',
